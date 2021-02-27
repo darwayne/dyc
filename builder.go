f540913f134d42e1cac4cf51da8942f4bfe30118
Darwayne <darwaynelynch@gmail.com>
2021-02-27 16:34:46 -0500

    adds ability to parse result from builder

--- builder.go
+++ builder.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
 // Builder allows you to build dynamo queries in a more convenient fashion
@@ -31,6 +32,7 @@ type Builder struct {
 	ascending           *bool
 	consistent          *bool
 	client              *Client
+	result              interface{}
 }
 
 // NewBuilder creates a new builder
@@ -212,6 +214,13 @@ func (s *Builder) toInQuery(column string, vals ...interface{}) string {
 	return builder.String()
 }
 
+// Result decodes the final result into the provided variable. Provided variable must be a pointer
+func (s *Builder) Result(val interface{}) *Builder {
+	s.result = val
+
+	return s
+}
+
 func (s *Builder) update(fn func()) *Builder {
 	if s.err != nil {
 		return s
@@ -255,7 +264,41 @@ func (s *Builder) GetItem(ctx context.Context) (*dynamodb.GetItemOutput, error) 
 	input, _ := s.ToGet()
 	output, err := s.client.GetItemWithContext(ctx, &input)
 
-	return output, err
+	return output, s.parseResult(input, err)
+}
+
+func (s *Builder) parseResult(result interface{}, errs ...error) error {
+	if len(errs) > 0 && errs[0] != nil {
+		return errs[0]
+	}
+
+	var err error
+	if s.result == nil {
+		return nil
+	}
+
+	rv := reflect.ValueOf(s.result)
+	if rv.Kind() != reflect.Ptr {
+		return ErrNotPointer
+	}
+
+	rv = rv.Elem()
+	switch rv.Kind() {
+	case reflect.Array, reflect.Slice:
+		raw, ok := result.([]map[string]*dynamodb.AttributeValue)
+		if !ok {
+			return ErrUnsupportedType
+		}
+		err = dynamodbattribute.UnmarshalListOfMaps(raw, s.result)
+	default:
+		raw, ok := result.(map[string]*dynamodb.AttributeValue)
+		if !ok {
+			return ErrUnsupportedType
+		}
+		err = dynamodbattribute.UnmarshalMap(raw, s.result)
+	}
+
+	return err
 }
 
 // UpdateItem builds and runs an update query
@@ -320,7 +363,7 @@ func (s *Builder) QueryAll(ctx context.Context) ([]map[string]*dynamodb.Attribut
 		return nil
 	})
 
-	return results, err
+	return results, s.parseResult(results, err)
 }
 
 // QuerySingle returns a single result matching the built query
@@ -349,7 +392,7 @@ func (s *Builder) QuerySingle(ctx context.Context) (map[string]*dynamodb.Attribu
 		err = nil
 	}
 
-	return result, err
+	return result, s.parseResult(result, err)
 }
 
 // ScanIterate allows you to query dynamo based on the built object.
